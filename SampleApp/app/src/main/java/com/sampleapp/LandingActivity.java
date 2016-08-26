7b62d8fbc8d32951d9aa7398910f00d52c74ccc9
Prakash Karuppannan Govindaraj <prakashkarupannan.govindaraj@impigertech.com>
2016-08-26 13:09:08 +0530

    Added Home Screen

--- SampleApp/app/src/main/java/com/sampleapp/LandingActivity.java
+++ SampleApp/app/src/main/java/com/sampleapp/LandingActivity.java
@@ -1,16 +1,26 @@
 package com.sampleapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by prakash on 26/08/16.
  */
-public class LandingActivity extends Activity {
+public class LandingActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(LandingActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
